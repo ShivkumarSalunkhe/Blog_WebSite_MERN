ad3f7182dc29bb57416f8719ab78a355abb2aa52
Shivkumar <shivkumarsalunkhe50gmail.com>
2022-12-28 23:39:40 +0530

    login

--- frontend/src/pages/login/Regsiter.jsx
+++ frontend/src/pages/login/Regsiter.jsx
@@ -18,7 +18,7 @@ export const Regsiter = () => {
         email,
         password,
       })
-      res.data && window.location.replace("/login")
+      res.data && window.location.replace("https://blog-website-mern.onrender.com/login")
     } catch (error) {
       setError(true)
     }
